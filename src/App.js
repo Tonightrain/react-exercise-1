32624d5c0b8c504b91a8c40d9967d0c23249b3b6
MSI-PV <805560812@qq.com>
2020-08-13 14:20:39 +0800

    exercise1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
+import About from './compoments/About';
+import Educations from './compoments/Educations';
+import Header from './compoments/Header';
 
 class App extends Component {
   render() {
-    return <main className="app">start here</main>;
+    return (
+      <main className="app">
+        <Header />
+        <About />
+        <Educations />
+      </main>
+    );
   }
 }
 
